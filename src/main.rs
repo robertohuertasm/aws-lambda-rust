aaf0c296bf99b8b63a2d99f9f04b381cf548cb42
Roberto Huertas <roberto.huertas@outlook.com>
2018-12-03 00:56:49 +0100

    commented code

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,17 @@ extern crate serde_json;
 use lambda::error::HandlerError;
 use std::error::Error;
 
+// Struct that will hold information of the request.
+// When we use an API Gateway as a proxy, which is the default
+// behaviour when we create it from the Lambda website, the request
+// will have a specific format with many different parameters.
+// We're only going to use `queryStringParameters` to check the
+// query string parameters (normally for GET requests) and `body`
+// to check for messages usually coming from POST requests.
 #[derive(Deserialize, Clone)]
 struct CustomEvent {
+    // note that we're using serde to help us to change
+    // the names of parameters accordingly to conventions.
     #[serde(rename = "queryStringParameters")]
     query_string_parameters: Option<QueryString>,
     body: Option<String>,
@@ -29,6 +38,12 @@ struct Body {
     first_name: Option<String>,
 }
 
+// Struct used for our function's response.
+// Note again that we're using `serde`.
+// It's also important to notice that you will need to explicitely
+// inform these properties for our API Gateway to work.
+// If you miss some of these properties you will likely get
+// a 502 error.
 #[derive(Serialize, Clone)]
 struct CustomOutput {
     #[serde(rename = "isBase64Encoded")]
@@ -38,6 +53,7 @@ struct CustomOutput {
     body: String,
 }
 
+// Just a static method to help us build the `CustomOutput`.
 impl CustomOutput {
     fn new(body: String) -> Self {
         CustomOutput {
@@ -48,12 +64,20 @@ impl CustomOutput {
     }
 }
 
+// This is our function entry point.
+// Note the use of the `lambda!` macro. It will hold our handler:
+// pub type Handler<E, O> = fn(E, Context) -> Result<O, HandlerError>
 fn main() -> Result<(), Box<dyn Error>> {
     // simple_logger::init_with_level(log::Level::Info)?;
     lambda!(my_handler);
     Ok(())
 }
 
+// Our handler will just check for a query string parameter called `firstName`.
+// Note the different behavior depending on the value of the parameter.
+// In case there's no query string parameter, we'll check the body of the request.
+// The body comes as a string so we'll have to use `Serde` again to deserialize it.
+// Finally, if we have no body, we'll return a default response.
 fn my_handler(e: CustomEvent, c: lambda::Context) -> Result<CustomOutput, HandlerError> {
     // checking the query string
     if let Some(q) = e.query_string_parameters {
