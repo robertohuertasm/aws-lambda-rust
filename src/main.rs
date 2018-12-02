a8cf71734930a64d9c634d35d30d43d3ef0457ec
Roberto Huertas <roberto.huertas@outlook.com>
2018-12-02 16:52:54 +0100

    body working

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,19 @@
 extern crate lambda_runtime as lambda;
 #[macro_use]
 extern crate serde_derive;
+extern crate serde_json;
 #[macro_use]
 extern crate log;
 extern crate simple_logger;
 
 use lambda::error::HandlerError;
-
 use std::error::Error;
 
 #[derive(Deserialize, Clone)]
 struct CustomEvent {
     #[serde(rename = "queryStringParameters")]
     query_string_parameters: Option<QueryString>,
-    body: Option<Body>,
+    body: Option<String>,
 }
 
 #[derive(Deserialize, Clone)]
@@ -58,34 +58,56 @@ fn my_handler(e: CustomEvent, c: lambda::Context) -> Result<CustomOutput, Handle
     if let Some(q) = e.query_string_parameters {
         if let Some(first_name) = q.first_name {
             return match first_name.as_ref() {
-                "" => Ok(CustomOutput::new(
-                    "Hello from Rust, my dear default user with empty qs parameter!".to_owned(),
-                )),
-                "error" => Err(c.new_error("Empty first name. Forced error. Qs.")),
+                "" => Ok(CustomOutput::new(format!(
+                    "Hello from Rust, my dear default user with empty parameter! (qs)"
+                ))),
+                "error" => Err(c.new_error("Empty first name (qs)")),
                 _ => Ok(CustomOutput::new(format!(
-                    "Hello from Rust, my dear {} (qs)!",
+                    "Hello from Rust, my dear {}! (qs)",
                     first_name
                 ))),
             };
         }
     }
     if let Some(b) = e.body {
-        if let Some(first_name) = b.first_name {
-            return match first_name.as_ref() {
-                "" => Ok(CustomOutput::new(
-                    "Hello from Rust, my dear default user with empty body!".to_owned(),
-                )),
-                "error" => Err(c.new_error("Empty first name. Forced error. Body.")),
-                _ => Ok(CustomOutput::new(format!(
-                    "Hello from Rust, my dear {} (body)!",
-                    first_name
-                ))),
-            };
-        }
+        //  if let Some(first_name) = b.first_name {
+        return match b.as_ref() {
+            "" => Ok(CustomOutput::new(format!(
+                "Hello from Rust, my dear default user with empty parameter! (body)"
+            ))),
+            "error" => Err(c.new_error("Empty first name (body)")),
+            _ => {
+                let result: Body = serde_json::from_str(&b).unwrap();
+                Ok(CustomOutput::new(format!(
+                    "Hello from Rust, my dear {}! (body)",
+                    result.first_name.unwrap_or("no param".to_owned())
+                )))
+            }
+        };
+        // }
     }
     Ok(CustomOutput {
         is_base64_encoded: false,
         status_code: 200,
-        body: format!("Hello from Rust, my dear default user! No body or qs"),
+        body: format!("Hello from Rust, my dear default user! No parameters"),
     })
 }
+
+// fn my_handler(e: CustomEvent, c: lambda::Context) -> Result<CustomOutput, HandlerError> {
+//     if let Some(x) = e.first_name {
+//         let x = x.as_str();
+//         match x {
+//             "" => Ok(CustomOutput {
+//                 message: format!("Hello from Rust, my dear default user with empty parameter!"),
+//             }),
+//             "error" => Err(c.new_error("Empty first name")),
+//             _ => Ok(CustomOutput {
+//                 message: format!("Hello from Rust, my dear {}!", x),
+//             }),
+//         }
+//     } else {
+//         Ok(CustomOutput {
+//             message: format!("Hello from Rust, my dear default user!"),
+//         })
+//     }
+// }
