a85cbda9979f45a3db6268907d69db3b4bf0afd1
Roberto Huertas <roberto.huertas@outlook.com>
2018-12-02 17:23:28 +0100

    no logs for the moment

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ extern crate lambda_runtime as lambda;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
-#[macro_use]
-extern crate log;
-extern crate simple_logger;
+// #[macro_use]
+// extern crate log;
+// extern crate simple_logger;
 
 use lambda::error::HandlerError;
 use std::error::Error;
@@ -49,7 +49,7 @@ impl CustomOutput {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    simple_logger::init_with_level(log::Level::Info)?;
+    // simple_logger::init_with_level(log::Level::Info)?;
     lambda!(my_handler);
     Ok(())
 }
